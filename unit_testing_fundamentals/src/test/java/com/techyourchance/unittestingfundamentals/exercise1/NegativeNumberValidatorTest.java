46288c90b473878d7a9671183cc10f772eba8078
wamaeb <benson.wamae@kopokopo.com>
2019-08-30 22:37:59 +0300

    Exercise One

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,33 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+
 public class NegativeNumberValidatorTest {
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setup(){
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void isPositiveNumber(){
+        Assert.assertThat(SUT.isNegative(1), is(false));
+    }
+
+    @Test
+    public void isZero(){
+        Assert.assertThat(SUT.isNegative(0),is(false));
+    }
+
+    @Test
+    public void isNegativeNumber(){
+        Assert.assertThat(SUT.isNegative(-1),is(true));
+    }
+
 
 }
